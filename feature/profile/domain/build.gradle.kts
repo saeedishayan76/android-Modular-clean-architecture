27ae0bbe6fdf4ccdf9766ffbd94d2ace67385df8
saeedishayan76 <shsaeedi156@gmail.com>
2023-12-18 03:06:42 +0330

    develop in process

---
+++ feature/profile/domain/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java-library")
+    id ("kotlin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+dependencies {
+    implementation(libs.kotlinx.coroutines.android)
+    implementation (libs.javax.inject)
+
+}
